dd2da8e8e9cb0c1461f7d55f6d972d544b19d7d8
Praneetha35 <praneethacp35@gmail.com>
2022-01-10 18:23:21 +0530

    custom-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import useAxios from "./customhooks/useAxios";
 
 function App() {
+  const [postId, setPostId] = useState(9);
+  const { response, loading, error, sendData } = useAxios({
+    method: "get",
+    url: `/public/v1/users/${postId}`,
+    //Accepting any type of data from the backend
+    headers: {
+      accept: "*/*",
+    },
+  });
+  console.log(response);
+
+  const getNextPost = () => {
+    setPostId(postId + 1);
+    sendData();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error.message}</p>}
+      <h3>Name: {response?.data?.data?.name}</h3>
+      <h3>id: {response?.data?.data?.id}</h3>
+      <h3>Gender: {response?.data?.data?.gender}</h3>
+      <h3>Email: {response?.data?.data?.email}</h3>
+      <button onClick={() => getNextPost()}>Next </button>
     </div>
   );
 }
